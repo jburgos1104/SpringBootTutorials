f0d1c8a7d72a88b1f3d3b0befac86826731d3513
Jose Miguel Burgos Cuartas <158490111+jburgos1104@users.noreply.github.com>
2025-02-25 20:01:10 -0500

    Tutorial-SpringBoot-(Modelos)

---
+++ tutorial/src/main/java/com/docencia/tutorial/repositories/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.docencia.tutorial.repositories;
+
+import com.docencia.tutorial.models.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
