c4bb21abc6d80fe18ab81dc1408e296c33156870
Jose Miguel Burgos Cuartas <158490111+jburgos1104@users.noreply.github.com>
2025-04-27 22:45:13 -0500

    Tutorial 12

---
+++ tutorial12/src/main/java/com/tutorial12/tutorial12/Tutorial12Application.java
@@ -0,0 +1,13 @@
+package com.tutorial12.tutorial12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial12Application.class, args);
+	}
+
+}
